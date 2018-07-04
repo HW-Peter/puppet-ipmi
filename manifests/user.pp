d963fd731f5da87f75363d863774367702c021c5
HW-Peter <HW-Peter@users.noreply.github.com>
2018-07-04 13:18:35 -0700

    Update user.pp

--- manifests/user.pp
+++ manifests/user.pp
@@ -36,22 +36,14 @@ define ipmi::user (
   exec { "ipmi_user_priv_${title}":
     command => "/usr/bin/ipmitool user priv ${user_id} ${priv} 1",
     unless  => "/usr/bin/test \"$(ipmitool user list 1 | grep '^${user_id}' | awk '{print \$6}')\" = ${privilege}",
-    notify  => [Exec["ipmi_user_enable_${title}"], Exec["ipmi_user_enable_sol_${title}"], Exec["ipmi_user_channel_setaccess_${title}"]],
+    notify  => [Exec["ipmi_user_enable_${title}"]],
   }
 
   exec { "ipmi_user_setpw_${title}":
     command => "/usr/bin/ipmitool user set password ${user_id} \'${password}\'",
     unless  => "/usr/bin/ipmitool user test ${user_id} 16 \'${password}\'",
-    notify  => [Exec["ipmi_user_enable_${title}"], Exec["ipmi_user_enable_sol_${title}"], Exec["ipmi_user_channel_setaccess_${title}"]],
+    notify  => [Exec["ipmi_user_enable_${title}"]],
   }
 
-  exec { "ipmi_user_enable_sol_${title}":
-    command     => "/usr/bin/ipmitool sol payload enable 1 ${user_id}",
-    refreshonly => true,
-  }
 
-  exec { "ipmi_user_channel_setaccess_${title}":
-    command     => "/usr/bin/ipmitool channel setaccess 1 ${user_id} callin=on ipmi=on link=on privilege=${priv}",
-    refreshonly => true,
-  }
 }
